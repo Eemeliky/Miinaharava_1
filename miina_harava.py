681d32acf301783fde7f0e95e7aaac3d84614b1a
Eemeli Kyröläinen <eemeli.ky@gmail.com>
2023-10-21 23:07:11 +0300

    Added function to ask user the difficulty level of the game and function to make a custom game + removed some reduntant global variables

--- miina_harava.py
+++ miina_harava.py
@@ -1,27 +1,26 @@
 import random
 import haravasto
 
-peli_data = {
+pelidata = {
     "kentta": [],
     "kansi": [],
     "miinat": 0,
-    "kentan_leveys": 9,
-    "kentan_korkeus": 9
+    "kentan_leveys": 0,
+    "kentan_korkeus": 0
 }
+# Vaikeus tasot: [leveys, korkeus, miinojen määrä]
+HELPPO = [9, 9, 10]
+NORMAALI = [16, 16, 40]
+VAIKEA = [30, 16, 99]
 
-
-KENTTA_LEVEYS = 9
-KENTTA_KORKEUS = 9
-SPRITE_SIVU = 40  # muuta jos käytät eri kokoisia spritejä (vakio 40x40px).
-IKKUNAN_LEVEYS = SPRITE_SIVU * peli_data["kentan_leveys"]
-IKKUNAN_KORKEUS = SPRITE_SIVU * peli_data["kentan_korkeus"]
+SPRITE_SIVU = 40  # Vakio spriten koko (40x40)px
 
 
 def numeroi():
     """
     Laskee jokaisen ruudun arvon sen vieressä olevien miinojen mukaan.
     """
-    kentta = peli_data["kentta"]
+    kentta = pelidata["kentta"]
     y_raja = len(kentta) - 1
     x_raja = len(kentta[0]) - 1
 
@@ -55,24 +54,24 @@ def numeroi():
                 kentta[y][x] += 1
 
 
-def alusta_peli():
+def luo_kentta():
     """
     alustaa  tyhjän miina kentän
     :return:
     """
     kentta, kansi, tyhjat_ruudut = ([] for _ in range(3))
 
-    for y in range(KENTTA_KORKEUS):
+    for y in range(pelidata["kentan_korkeus"]):
         kentta.append([])
         kansi.append([])
-        for x in range(KENTTA_LEVEYS):
+        for x in range(pelidata["kentan_leveys"]):
             kentta[-1].append(0)
             kansi[-1].append(0)
             tyhjat_ruudut.append((x, y))
 
-    peli_data["kentta"] = kentta
-    peli_data["kansi"] = kansi
-    peli_data["tyhjat"] = tyhjat_ruudut
+    pelidata["kentta"] = kentta
+    pelidata["kansi"] = kansi
+    pelidata["tyhjat"] = tyhjat_ruudut
 
 
 def luo_turva_alue(aloitus_x, aloitus_y):
@@ -82,9 +81,9 @@ def luo_turva_alue(aloitus_x, aloitus_y):
     :param aloitus_y: Pelin aloitus ruudun y koordinaatti
     :return: Listan, joka sisältää koordinaatti parit aloitus ruudun ympärillä olevista ruuduista
     """
-    peli_data["tyhjat"].remove((aloitus_x, aloitus_y))
+    pelidata["tyhjat"].remove((aloitus_x, aloitus_y))
     turva_alue = []
-    for k_pari in peli_data["tyhjat"]:
+    for k_pari in pelidata["tyhjat"]:
         x, y = k_pari
 
         # vas ylä
@@ -120,7 +119,7 @@ def luo_turva_alue(aloitus_x, aloitus_y):
             turva_alue.append((x, y))
 
     for ruutu in turva_alue:
-        peli_data["tyhjat"].remove(ruutu)
+        pelidata["tyhjat"].remove(ruutu)
 
     return turva_alue
 
@@ -132,17 +131,17 @@ def miinoita(turva_alue):
     """
 
     luku = 0
-    while luku < peli_data["miinat"]:
-        if not peli_data["tyhjat"]:
+    while luku < pelidata["miinat"]:
+        if not pelidata["tyhjat"]:
             x, y = turva_alue.pop()
-            peli_data["kentta"][y][x] = -1
+            pelidata["kentta"][y][x] = -1
         else:
-            idx = random.randint(0, len(peli_data["tyhjat"]) - 1)
-            x, y = peli_data["tyhjat"].pop(idx)
-            peli_data["kentta"][y][x] = -1
+            idx = random.randint(0, len(pelidata["tyhjat"]) - 1)
+            x, y = pelidata["tyhjat"].pop(idx)
+            pelidata["kentta"][y][x] = -1
         luku += 1
 
-    peli_data["tyhjat"] = []
+    pelidata["tyhjat"] = []
 
 
 def piirra_kentta():
@@ -154,31 +153,31 @@ def piirra_kentta():
     haravasto.tyhjaa_ikkuna()
     haravasto.piirra_tausta()
     haravasto.aloita_ruutujen_piirto()
-    for y, rivi in enumerate(peli_data['kentta']):
+    for y, rivi in enumerate(pelidata['kentta']):
         for x, asia in enumerate(rivi):
-            if peli_data['kansi'][y][x] == 9:
+            if pelidata['kansi'][y][x] == 9:
                 haravasto.lisaa_piirrettava_ruutu('f',
                                                   x * SPRITE_SIVU,
-                                                  IKKUNAN_KORKEUS - (SPRITE_SIVU * (y + 1)))
-            elif peli_data['kansi'][y][x] == 1:
+                                                  pelidata["kentan_korkeus"] - (SPRITE_SIVU * (y + 1)))
+            elif pelidata['kansi'][y][x] == 1:
                 if asia == -1:
                     haravasto.lisaa_piirrettava_ruutu('x',
                                                       x * SPRITE_SIVU,
-                                                      IKKUNAN_KORKEUS - (SPRITE_SIVU * (y + 1)))
+                                                      pelidata["kentan_korkeus"] - (SPRITE_SIVU * (y + 1)))
                 else:
                     haravasto.lisaa_piirrettava_ruutu(str(asia),
                                                       x * 40,
-                                                      IKKUNAN_KORKEUS - (SPRITE_SIVU * (y + 1)))
+                                                      pelidata["kentan_korkeus"] - (SPRITE_SIVU * (y + 1)))
             else:
                 haravasto.lisaa_piirrettava_ruutu(" ",
                                                   x * 40,
-                                                  IKKUNAN_KORKEUS - (SPRITE_SIVU * (y + 1)))
+                                                  pelidata["kentan_korkeus"] - (SPRITE_SIVU * (y + 1)))
 
-    if "game_over" in peli_data:
+    if "game_over" in pelidata:
         if "xr" in haravasto.grafiikka["kuvat"]:
             haravasto.lisaa_piirrettava_ruutu('xr',
-                                              peli_data["game_over"][0] * SPRITE_SIVU,
-                                              IKKUNAN_KORKEUS - (SPRITE_SIVU * (peli_data["game_over"][1] + 1)))
+                                              pelidata["game_over"][0] * SPRITE_SIVU,
+                                              pelidata["kentan_korkeus"] - (SPRITE_SIVU * (pelidata["game_over"][1]+1)))
     haravasto.piirra_ruudut()
 
 
@@ -187,8 +186,8 @@ def tulvataytto(aloitus_x, aloitus_y):
     Merkitsee planeetalla olevat tuntemattomat alueet turvalliseksi siten, että
     täyttö aloitetaan annetusta x, y -pisteestä.
     """
-    kentta = peli_data["kentta"]
-    kansi = peli_data["kansi"]
+    kentta = pelidata["kentta"]
+    kansi = pelidata["kansi"]
     fill_list = [(aloitus_x, aloitus_y)]
     y_raja = len(kentta)
     x_raja = len(kentta[0])
@@ -233,26 +232,26 @@ def tulvataytto(aloitus_x, aloitus_y):
 
 
 def nayta_miinat():
-    for y, rivi in enumerate(peli_data["kentta"]):
+    for y, rivi in enumerate(pelidata["kentta"]):
         for x, ruutu in enumerate(rivi):
             if ruutu == -1:
-                peli_data["kansi"][y][x] = 1
+                pelidata["kansi"][y][x] = 1
     print("GAME OVER!")
     print("Klikkaa minne tahansa pelikentällä lopettaaksesi pelin")
 
 
 def voitto_tarkistus():
     avaamattomat = 0
-    for y, rivi in enumerate(peli_data["kansi"]):
+    for y, rivi in enumerate(pelidata["kansi"]):
         for x, ruutu in enumerate(rivi):
             if ruutu == 0 or ruutu == 9:
                 avaamattomat += 1
-    if avaamattomat == peli_data["miinat"]:
-        for y, rivi in enumerate(peli_data["kansi"]):
+    if avaamattomat == pelidata["miinat"]:
+        for y, rivi in enumerate(pelidata["kansi"]):
             for x, ruutu in enumerate(rivi):
                 if ruutu == 0:
-                    peli_data["kansi"][y][x] = 9
-        peli_data["voitto"] = (0, 0)
+                    pelidata["kansi"][y][x] = 9
+        pelidata["voitto"] = (0, 0)
         print("VOITIT PELIN!!!")
         print("Klikkaa minne tahansa pelikentällä lopettaaksesi pelin")
 
@@ -263,24 +262,24 @@ def kasittele_hiiri(x, y, tapahtuma, _):
     Tulostaa hiiren sijainnin sekä painetun napin terminaaliin.
     """
     ruutu_x = x // SPRITE_SIVU
-    ruutu_y = (KENTTA_KORKEUS - 1) - (y // SPRITE_SIVU)
+    ruutu_y = (pelidata["kentan_korkeus"] - 1) - (y // SPRITE_SIVU)
 
-    if "voitto" in peli_data:
+    if "voitto" in pelidata:
         if tapahtuma == haravasto.HIIRI_VASEN:
             haravasto.lopeta()
-    elif "game_over" in peli_data:
+    elif "game_over" in pelidata:
         if tapahtuma == haravasto.HIIRI_VASEN:
             haravasto.lopeta()
 
     else:
         if tapahtuma == haravasto.HIIRI_VASEN:
-            if peli_data["kansi"][ruutu_y][ruutu_x] != 9:
+            if pelidata["kansi"][ruutu_y][ruutu_x] != 9:
 
-                if peli_data["kentta"][ruutu_y][ruutu_x] == -1:
-                    peli_data["game_over"] = (ruutu_x, ruutu_y)
+                if pelidata["kentta"][ruutu_y][ruutu_x] == -1:
+                    pelidata["game_over"] = (ruutu_x, ruutu_y)
                     nayta_miinat()
                 else:
-                    if peli_data["tyhjat"]:
+                    if pelidata["tyhjat"]:
                         turva_alue = luo_turva_alue(ruutu_x, ruutu_y)
                         miinoita(turva_alue)
                         numeroi()
@@ -290,29 +289,79 @@ def kasittele_hiiri(x, y, tapahtuma, _):
 
         elif tapahtuma == haravasto.HIIRI_OIKEA:
 
-            if peli_data["kansi"][ruutu_y][ruutu_x] == 0:
-                peli_data["kansi"][ruutu_y][ruutu_x] = 9
+            if pelidata["kansi"][ruutu_y][ruutu_x] == 0:
+                pelidata["kansi"][ruutu_y][ruutu_x] = 9
 
-            elif peli_data["kansi"][ruutu_y][ruutu_x] == 9:
-                peli_data["kansi"][ruutu_y][ruutu_x] = 0
+            elif pelidata["kansi"][ruutu_y][ruutu_x] == 9:
+                pelidata["kansi"][ruutu_y][ruutu_x] = 0
 
 
-def parametrien_syotto():
-    peli_data["miinat"] = peli_data["kentan_leveys"] * peli_data["kentan_korkeus"]
-    valid = False
-    while not valid:
+def aseta_vaikeustaso(vaikeustaso):
+    pelidata["kentan_leveys"] = vaikeustaso[0]
+    pelidata["kentan_korkeus"] = vaikeustaso[1]
+    pelidata["miinat"] = vaikeustaso[2]
+
+
+def luo_mukautettu_peli():
+    while True:
         try:
-            peli_data["miinat"] = int(input("Anna miinojen määrä: "))
-            if peli_data["kentan_leveys"] * peli_data["kentan_korkeus"] > peli_data["miinat"] >= 0:
-                valid = True
-            else:
-                print("Kentällä on oltava vähitään 1 tyhjä ruutu!")
-                print("sallittu miinojen määrä (0 - {a})"
-                      .format(a=peli_data["kentan_leveys"] * peli_data["kentan_korkeus"] - 1))
+            pelidata["kentan_leveys"] = int(input("Anna pelikentän leveys(max 100): "))
+        except ValueError:
+            print("Leveys täytyy olla kokonaisluku!")
+
+        if 101 > pelidata["kentan_leveys"] > 0:
+            break
+        else:
+            print("Leveys on oltava välillä (0 - 100)")
+
+    while True:
+        try:
+            pelidata["kentan_korkeus"] = int(input("Anna pelikentän korkeus(max 100): "))
+        except ValueError:
+            print("Korkeus täytyy olla kokonaisluku!")
+
+        if 101 > pelidata["kentan_korkeus"] > 0:
+            break
+        else:
+            print("Korkeus on oltava välillä (0 - 100)")
+
+    while True:
+        try:
+            pelidata["miinat"] = int(input("Anna miinojen määrä: "))
         except ValueError:
             print("Miinojen määrä on olta kokonaisluku!")
 
+        if pelidata["kentan_leveys"] * pelidata["kentan_korkeus"] > pelidata["miinat"] >= 0:
+            break
+        else:
+            print("Kentällä on oltava vähitään 1 tyhjä ruutu!")
+            print("sallittu miinojen määrä (0 - {a})"
+                  .format(a=pelidata["kentan_leveys"] * pelidata["kentan_korkeus"] - 1))
 
+
+def parametrien_syotto():
+    pelidata["pelaaja_nimi"] = input("Anna pelaaja nimi: ")
+
+    print("Vaikeustasot, Kentän koko, miinojen määrä:")
+    print(f"Helppo(h) = {HELPPO[0]}x{HELPPO[1]}, {HELPPO[2]} miinaa")
+    print(f"Normaali(n) = {NORMAALI[0]}x{NORMAALI[1]}, {NORMAALI[2]} miinaa")
+    print(f"Vaikea(v) = {VAIKEA[0]}x{VAIKEA[1]}, {VAIKEA[2]} miinaa")
+    print("Mukautettu(m) = NxN, ? miinaa")
+
+    while True:
+        valinta = input("Anna vaikeustaso: ").lower()
+        if valinta == "h" or valinta == "helppo":
+            aseta_vaikeustaso(HELPPO)
+            break
+        elif valinta == "n" or valinta == "normaali":
+            aseta_vaikeustaso(NORMAALI)
+            break
+        elif valinta == "v" or valinta == "vaikea":
+            aseta_vaikeustaso(VAIKEA)
+            break
+        elif valinta == "m" or valinta == "mukautettu":
+            luo_mukautettu_peli()
+            break
 
 
 def main():
@@ -320,9 +369,10 @@ def main():
     Lataa pelin grafiikat, luo peli-ikkunan ja asettaa siihen piirtokäsittelijän.
     """
     parametrien_syotto()
-    alusta_peli()
+    luo_kentta()
     haravasto.lataa_kuvat("spritet")
-    haravasto.luo_ikkuna(IKKUNAN_LEVEYS, IKKUNAN_KORKEUS)
+    haravasto.luo_ikkuna(SPRITE_SIVU * pelidata["kentan_leveys"],
+                         SPRITE_SIVU * pelidata["kentan_korkeus"])
     haravasto.aseta_hiiri_kasittelija(kasittele_hiiri)
     haravasto.aseta_piirto_kasittelija(piirra_kentta)
     haravasto.aloita()
